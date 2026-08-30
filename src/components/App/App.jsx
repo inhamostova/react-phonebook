c147e57f8f5c48e215ee6f5bd46c1ef150e426ad
inhamostova <inha.mostova@gmail.com>
2026-08-30 23:57:09 +0300

    add locale-storage service

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,35 @@
 import { Component } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/ContactList';
+import { Filter } from '../Filter/Filter';
+import { Container } from './App.styled';
+
+const initContatcs = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initContatcs,
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   filterChange = evt => {
     this.setState({ filter: evt.target.value });
@@ -51,7 +68,7 @@ export class App extends Component {
     );
 
     return (
-      <div>
+      <Container>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={addContact} />
 
@@ -59,7 +76,7 @@ export class App extends Component {
         <Filter value={filter} onChange={filterChange} />
 
         <ContactList contacts={visibleContacts} onDelete={deleteContact} />
-      </div>
+      </Container>
     );
   }
 }
